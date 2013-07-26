32b3af60bd6177f969788cedb4d8f6f1c6a79c10
Danilo Bargen <gezuru@gmail.com>
2013-07-26 10:59:37 +0200

    Initial commit

---
+++ gen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mvn archetype:generate -DarchetypeArtifactId=android-with-test -DarchetypeGroupId=de.akquinet.android.archetypes -DarchetypeVersion=1.0.9 -DgroupId=ch.dbrgn -DartifactId=dabs -Dplatform=14
